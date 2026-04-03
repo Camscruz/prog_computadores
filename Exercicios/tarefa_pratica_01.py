0bc44db1e5061ae7d5a19461b452673926abc323
Camscruz <camille.cruz@id.uff.br>
2026-04-03 20:02:22 -0300

    Exercícios

---
+++ Exercicios/tarefa_pratica_01.py
@@ -0,0 +1,52 @@
+# Nome: Camille Cardoso Cruz - Turma: J1
+# Tarefa prática 1
+
+#Para usar a biblioteca math encontrada no código é necessário primeiro realizar um comando para chamá-la
+
+import math
+
+a = 426027005
+b = 200000000
+c = 10
+
+k = (str(round((max(math.ceil(a/b), int(b/a), math.sin(a)*b/(a*math.cos(b)+1))) / c * 10 % 3 / c +
+math.log(a,10)/math.log(b), 4)))
+
+print(f"O valor de K é: {k}")
+
+
+import math
+
+a = 200000000
+b = 426027005
+c = 20
+
+k = (str(round((max(math.ceil(a/b), int(b/a), math.sin(a)*b/(a*math.cos(b)+1))) / c * 10 % 3 / c +
+math.log(a,10)/math.log(b), 4)))
+
+print(f"O valor de K é: {k}")
+
+
+import math
+
+a = 500000000
+b = 200000000
+c = 5
+
+k = (str(round((max(math.ceil(a/b), int(b/a), math.sin(a)*b/(a*math.cos(b)+1))) / c * 10 % 3 / c +
+math.log(a,10)/math.log(b), 4)))
+
+print(f"O valor de K é: {k}")
+
+
+#Além disso poderia ser utilizado o input para realizar a escolha em tempo real pelo usuário dos valores de a, b e c
+import math
+
+a = float(input("Insira o valor de a: "))
+b = float(input("Insira o valor de b: "))
+c = float(input("Insira o valor de c: "))
+
+k = (str(round((max(math.ceil(a/b), int(b/a), math.sin(a)*b/(a*math.cos(b)+1))) / c * 10 % 3 / c +
+math.log(a,10)/math.log(b), 4)))
+
+print(f"O valor de K é: {k}")
---
+++ Exercicios/tarefa_pratica_02.py
@@ -0,0 +1,34 @@
+# Nome: Camille Cardoso Cruz - Turma: J1
+# Tarefa prática 2
+
+# A expressão dada é:
+# t = x % y < z // x or x * y / 9 > z + x and z * y < x * w
+# Para x = 426027005; y = 7; z = 600000000; w = 17;
+# Para x = 426027005; y = 13; z = 500000000; w = 19;
+# Para x = 426027005; y = 16; z = 700000000; w = 12;
+
+#Para usar a biblioteca math encontrada no código é necessário primeiro realizar um comando para chamá-la
+
+import math
+
+x, y, z, w = 426027005, 7, 600000000, 17
+
+t = x % y < z // x or x * y / 9 > z + x and z * y < x * w
+
+print(f"O valor de t é: {t}")
+
+import math
+
+x, y, z, w = 426027005, 13, 500000000, 9
+
+t = x % y < z // x or x * y / 9 > z + x and z * y < x * w
+
+print(f"O valor de t é: {t}")
+
+import math
+
+x, y, z, w = 426027005, 16, 700000000, 12
+
+t = x % y < z // x or x * y / 9 > z + x and z * y < x * w
+
+print(f"O valor de t é: {t}")
