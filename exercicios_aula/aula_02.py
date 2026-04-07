739bd41f1ddfc1b1b92b67ad05e720c9c6a3c750
Camille Cruz <camille.cruz@external.cleveradvertising.com>
2026-04-07 16:48:48 -0300

    Tarefa aula prática 02

---
+++ exercicios_aula/aula_02.py
@@ -0,0 +1,12 @@
+# Nome: Camille Cardoso Cruz - Turma: J1
+# Aula prática 02
+
+# Escreva um programa que leia 2 números inteiros a e b e informe se a é divisível por b
+
+a=int(input("Informe um número inteiro a: "))
+b=int(input("Informe um número inteiro b: "))
+
+if (a%b==0):
+    print ("O número a é divisível por b.")
+else: 
+    print ("O número a não é divisível por b.")
